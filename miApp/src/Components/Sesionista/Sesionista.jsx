2de7d0cfa2a83c603f2c4dcd0393804354c1e825
Evaristo.Aguirre <dasbrotchen@gmail.com>
2023-02-14 17:03:39 -0300

    desarrollo-secciones_sesiones

--- miApp/src/Components/Sesionista/Sesionista.jsx
+++ miApp/src/Components/Sesionista/Sesionista.jsx
@@ -2,7 +2,28 @@ import React from 'react';
 
 const Sesionista = () => {
     return(
-        <div>Sesionista</div>
+
+        <>
+
+        <div className='sesiones-indice'>
+            <h2>Sesionista</h2>
+        </div>
+        <div className='sesiones-tarjeta'>
+            <h3>Grabamos juntxs?</h3>
+            <div className='sesiones-tarjeta-planes'>
+                <div className='sesiones-planes_izq'>Foto 1</div>
+                <div className='sesiones-planes_cen'>Foto 2</div>
+                <div className='sesiones-planes_der'>Foto 3</div>
+            </div>
+            <div className='sesiones-tarjeta-texto'>
+                <div className='sesiones-tarjeta-texto_izq'>Si estás buscando otro tipo de percusión para tu canción, podemos grabar de forma exclusiva para vos.</div>
+                <div className='sesiones-tarjeta-texto_der'>Elegí qué es lo que necesitás.</div>
+            </div>
+        </div>
+        
+        </>
+
+        
     )
 }
 
